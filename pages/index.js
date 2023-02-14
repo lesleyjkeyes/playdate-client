09b8fd66f92f2ab2a04b0d856757ca69455d9841
Lesley Keyes <lesleyjkeyes@gmail.com>
2023-02-13 22:22:24 -0500

    started pet card

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,13 @@
 import { Button } from 'react-bootstrap';
 import { signOut } from '../utils/auth';
 import { useAuth } from '../utils/context/authContext';
+import DogCard from '../components/DogCard';
 
 function Home() {
   const { user } = useAuth();
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
+    <div>
+      <DogCard />
       <h1>Hello {user.fbUser.displayName}! </h1>
       <p>Click the button below to logout!</p>
       <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
